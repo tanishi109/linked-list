e23ece3e6dfe24e001a92a1a56a2b8978dc9e435
tanishi109 <note109@gmail.com>
2017-11-17 00:11:19 +0900

    add Observer pattern by linked-list

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,8 +4,68 @@
 #include "stdafx.h"
 #include <stdlib.h>
 
+class Observer;
+class Subject
+{
+public:
+    Subject()
+    :  head_(NULL)
+    {}
+
+    void addObserver(Observer* observer);
+    void notify();
+private:
+    Observer* head_;
+};
+
+class Observer
+{
+    friend class Subject;
+public:
+    int value_;
+
+    Observer(int v)
+    : next_(NULL),
+      value_(v)
+    {}
+
+private:
+    Observer* next_;
+
+    void onNotify()
+    {
+        printf("notify! #%d\n", this->value_);
+    }
+};
+
+void Subject::addObserver(Observer* observer)
+{
+    observer->next_ = head_;
+    head_ = observer;
+}
+
+void Subject::notify()
+{
+    Observer* observer = head_;
+    while (observer != NULL)
+    {
+        observer->onNotify();
+        observer = observer->next_;
+    }
+}
+
 int main()
 {
+    Subject* sub = new Subject();
+
+    for (int i = 0; i < 10; i++)
+    {
+        Observer* ob = new Observer(i);
+        sub->addObserver(ob);
+    }
+
+    sub->notify();
+
     system("pause");
     return 0;
 }
